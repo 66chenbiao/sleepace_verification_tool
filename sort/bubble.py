68a527ce8687277d2b9d85e4a4c196b9fd99bb86
Evan <44183747+66chenbiao@users.noreply.github.com>
2021-06-04 18:18:57 +0800

    Update bubble.py

--- sort/bubble.py
+++ sort/bubble.py
@@ -1,35 +1,50 @@
-import unittest
-
-def bubble_sort(list):
-    """Sort list using Bubble Sort algorithm
-    
-    Arguments:
-        list {integer} -- Unsorted list
-    
-    Returns:
-        list {integer} -- Sorted list
+def bubble_sort(collection):
+    """Pure implementation of bubble sort algorithm in Python
+    :param collection: some mutable ordered collection with heterogeneous
+    comparable items inside
+    :return: the same collection ordered by ascending
+    Examples:
+    >>> bubble_sort([0, 5, 2, 3, 2])
+    [0, 2, 2, 3, 5]
+    >>> bubble_sort([0, 5, 2, 3, 2]) == sorted([0, 5, 2, 3, 2])
+    True
+    >>> bubble_sort([]) == sorted([])
+    True
+    >>> bubble_sort([-2, -45, -5]) == sorted([-2, -45, -5])
+    True
+    >>> bubble_sort([-23, 0, 6, -4, 34]) == sorted([-23, 0, 6, -4, 34])
+    True
+    >>> bubble_sort(['d', 'a', 'b', 'e', 'c']) == sorted(['d', 'a', 'b', 'e', 'c'])
+    True
+    >>> import random
+    >>> collection = random.sample(range(-50, 50), 100)
+    >>> bubble_sort(collection) == sorted(collection)
+    True
+    >>> import string
+    >>> collection = random.choices(string.ascii_letters + string.digits, k=100)
+    >>> bubble_sort(collection) == sorted(collection)
+    True
     """
+    length = len(collection)
+    for i in range(length - 1):
+        swapped = False
+        for j in range(length - 1 - i):
+            if collection[j] > collection[j + 1]:
+                swapped = True
+                collection[j], collection[j + 1] = collection[j + 1], collection[j]
+        if not swapped:
+            break  # Stop iteration if the collection is sorted.
+    return collection
 
-    swap = True
-    while swap:
-        swap = False
-        for n in range(len(list) - 1 ):
-            if list[n] > list[n+1]:
-                current = list[n]
-                list[n] = list[n + 1]
-                list[n + 1] = current
-                swap = True
-    return list
 
-class TestBubbleSort(unittest.TestCase):
+if __name__ == "__main__":
+    import doctest
+    import time
 
-    def test_sort(self):
-        self.assertListEqual(bubble_sort([1, 1]), [1, 1])
-        self.assertListEqual(bubble_sort([1, 2]), [1, 2])
-        self.assertListEqual(bubble_sort([2, 1]), [1, 2])
-        self.assertListEqual(bubble_sort([6, 3, 9, 1, 43]), [1, 3, 6, 9, 43])
-        self.assertListEqual(bubble_sort([14, 46, 43, 27, 57, 41, 45, 21, 70]), [14, 21, 27, 41, 43, 45, 46, 57, 70])
+    doctest.testmod()
 
-if __name__ == '__main__':
-    unittest.main()
-
+    user_input = input("Enter numbers separated by a comma:").strip()
+    unsorted = [int(item) for item in user_input.split(",")]
+    start = time.process_time()
+    print(*bubble_sort(unsorted), sep=",")
+    print(f"Processing time: {time.process_time() - start}")
