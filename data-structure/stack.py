2e182d0dbd6336c344452be58ea210453e19fbd4
Evan <44183747+66chenbiao@users.noreply.github.com>
2021-06-10 20:52:28 +0800

    Update stack.py

--- data-structure/stack.py
+++ data-structure/stack.py
@@ -1,39 +1,39 @@
 import unittest
 
+
 class Stack:
-    """Simple Stack implementation - First In Last Out
-    """
+    """Simple Stack implementation - First In Last Out"""
 
     def __init__(self):
         self.__data = []
 
     def push(self, text):
         """Push an item to a stack
-        
+
         Arguments:
             text {string} -- Element that will be put on stack
         """
         self.__data.insert(0, text)
 
     def pop(self):
-        """Get an element from a top of a stack 
-        
+        """Get an element from a top of a stack
+
         Returns:
             string -- element from a top of a stack
         """
         if len(self.__data) == 0:
             return None
-        
+
         popped = self.__data[0]
         new_data = []
         for index in range(1, len(self.__data)):
             new_data.append(self.__data[index])
         self.__data = new_data
         return popped
-        
+
     def peek(self):
         """Check what currently is at a top of a stack without taking it off
-        
+
         Returns:
             string -- element from a top of a stack
         """
@@ -41,27 +41,26 @@ class Stack:
 
 
 class StackTest(unittest.TestCase):
-
     def test_push_one_peek_one(self):
         stack = Stack()
-        stack.push('one')
-        self.assertEqual(stack.peek(), 'one', "Should be 'one'")
+        stack.push("one")
+        self.assertEqual(stack.peek(), "one", "Should be 'one'")
 
     def test_push_three_peek_last(self):
         stack = Stack()
-        stack.push('one')
-        stack.push('two')
-        stack.push('three')
-        self.assertEqual(stack.peek(), 'three', "Should be 'three'")
+        stack.push("one")
+        stack.push("two")
+        stack.push("three")
+        self.assertEqual(stack.peek(), "three", "Should be 'three'")
 
     def test_push_three_pop_one_peek_second(self):
         stack = Stack()
-        stack.push('one')
-        stack.push('two')
-        stack.push('three')
+        stack.push("one")
+        stack.push("two")
+        stack.push("three")
         popped = stack.pop()
-        self.assertEqual(popped, 'three', "Should be 'three'")
-        self.assertEqual(stack.peek(), 'two', "Should be 'two'")
+        self.assertEqual(popped, "three", "Should be 'three'")
+        self.assertEqual(stack.peek(), "two", "Should be 'two'")
 
     def test_pop_empty_stack(self):
         stack = Stack()
@@ -69,5 +68,5 @@ class StackTest(unittest.TestCase):
         self.assertIsNone(popped)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
