103b57f6ff8d3729cfbc195283a9e7ba1d604dce
Evan <44183747+66chenbiao@users.noreply.github.com>
2021-06-10 20:50:06 +0800

    Update dna.py

--- science/bio/dna.py
+++ science/bio/dna.py
@@ -1,27 +1,29 @@
 import unittest
 
+
 def dna(sequence):
     """Finds complementary DNA sequence
-    
+
     Arguments:
         sequence {String} -- A DNA sequence, e.g `ATTGC`
-    
+
     Returns:
         [Stringe] -- Complementatry DNA sequence to given one
     """
     dna = []
-    dictionary = {'A': 'T', 'T':'A', 'G': 'C', 'C': 'G'}
+    dictionary = {"A": "T", "T": "A", "G": "C", "C": "G"}
     for n in sequence:
         dna.append(dictionary.get(n))
-    return ''.join(dna)
+    return "".join(dna)
+
 
 def dna_location(dna, sub):
     """Finds all indexes of DNA sub sequence in bigger DNA sequence
-    
+
     Arguments:
         dna {String} -- DNA sequence
         sub {String} -- DNA substring, to be found in bigger one
-    
+
     Returns:
         list[integer] -- List of occurence indexes of DNA substring
     """
@@ -34,19 +36,20 @@ def dna_location(dna, sub):
         if i != -1:
             index += i
             dna_locations.append(index)
-            dna = dna[i+sub_length:]
+            dna = dna[i + sub_length :]
             index += sub_length
         else:
             check = False
     return dna_locations
 
+
 def hamming_distance(first, second):
     """Calculate Hamming Distance, number of mismatched nucleotides in two DNA sequences
-    
+
     Arguments:
         first {String} -- First DNA sequence
         second {String} -- Seconde DNA sequence
-    
+
     Returns:
         integer -- Number of mismatches
     """
@@ -56,25 +59,31 @@ def hamming_distance(first, second):
             sum += 1
     return sum
 
+
 class TestDna(unittest.TestCase):
-    
     def test_complementary_dna(self):
-        self.assertEqual(dna('ATTGC'), 'TAACG')
-        self.assertEqual(dna('GTAT'), 'CATA')
+        self.assertEqual(dna("ATTGC"), "TAACG")
+        self.assertEqual(dna("GTAT"), "CATA")
 
     def test_substring_locations_in_dna(self):
-        self.assertEqual(dna_location('AUGCUUCAGAAAGGUCUUACG', 'ATAT'), [])
-        self.assertEqual(dna_location('AUGCUUCAGAAAGGUCUUACG', 'UGCU'), [2])
-        self.assertEqual(dna_location('GATATATGCATATACTT', 'ATAT'), [2, 10])
-        self.assertEqual(dna_location('AGCTTTTCATTCTGAGTGCAACGGGCAATAAGAGTGTCTGATGAGTATC', 'GAGT'), [14, 32, 43])
+        self.assertEqual(dna_location("AUGCUUCAGAAAGGUCUUACG", "ATAT"), [])
+        self.assertEqual(dna_location("AUGCUUCAGAAAGGUCUUACG", "UGCU"), [2])
+        self.assertEqual(dna_location("GATATATGCATATACTT", "ATAT"), [2, 10])
+        self.assertEqual(
+            dna_location("AGCTTTTCATTCTGAGTGCAACGGGCAATAAGAGTGTCTGATGAGTATC", "GAGT"),
+            [14, 32, 43],
+        )
 
     def test_hamming_distance(self):
-        self.assertEqual(hamming_distance('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCT'), 7)
+        self.assertEqual(hamming_distance("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT"), 7)
         self.assertEqual(
             hamming_distance(
-                'TGACCCGTTATGCTCGAGTTCGGTCAGAGCGTCATTGCGAGTAGTCGTTTGCTTTCTCAAACTCC',
-                'GAGCGATTAAGCGTGACAGCCCCAGGGAACCCACAAAACGTGATCGCAGTCCATCCGATCATACA'), 50)
+                "TGACCCGTTATGCTCGAGTTCGGTCAGAGCGTCATTGCGAGTAGTCGTTTGCTTTCTCAAACTCC",
+                "GAGCGATTAAGCGTGACAGCCCCAGGGAACCCACAAAACGTGATCGCAGTCCATCCGATCATACA",
+            ),
+            50,
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
